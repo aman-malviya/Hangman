3c4edc4c7d228399ddedd479266ac459b5ff9744
Aman <amanmalviya14032000@gmail.com>
2021-05-18 00:40:20 +0530

    6 tasks done

--- hangman.py
+++ hangman.py
@@ -8,28 +8,13 @@ Important instruction
 '''
 
 def is_word_guessed(secret_word, letters_guessed):
-    '''
-    secret_word: word guess by the user
-    letters_guessed: list hold all the word guess by the user
-    returns: 
-      return True (if user guess the world correctly )
-      return False (wrong selection)
-    '''
+    if secret_word==letters_guessed:
+        return True
     return False
 
-# if you want to test this function please call function -> get_guessed_word("kindness", [k, n, d])
 
 
 def get_guessed_word(secret_word, letters_guessed):
-    '''
-    secret_word: word guess by the user
-    letters_guessed: list hold all the word guess by the user
-    returns: 
-      return string which contain all the right guessed characters
-      Example :- 
-      if secret_word -> "kindness" and letters_guessed = [k, n, s]
-      return "k_n_n_ss"
-    '''
     index = 0
     guessed_word = ""
     while (index < len(secret_word)):
@@ -42,59 +27,57 @@ def get_guessed_word(secret_word, letters_guessed):
 
 
 def get_available_letters(letters_guessed):
-    '''
-    letters_guessed: list contains all guessed characters
-    returns: 
-      it return string which contains all characters except guessed letters
-    Example :-
-      letters_guessed = ['e', 'a'] then    
-      return sting is -> `bcdfghijklmnopqrstuvwxyz`
-    '''
     letters_left = string.ascii_lowercase
+    for letter in letters_guessed:
+        if letter in letters_left:
+            letters_left=letters_left.replace(letter, "")
     return letters_left
+
+def print_image(image):
+    print(IMAGES[image])
+
+def isValid(character):
+    if len(character)==1 and 'a'<=character<='z':
+        return True
+    return False
 
 
 def hangman(secret_word):
-    '''
-    secret_word (string) : secret word to guessed by the user.
-
-    Steps to start Hangman:
-
-    * In the beginning of the game user will know about the total characters in the secret_word    
-
-    * In each round user will guess one character 
-
-    * After each character give feedback to the user
-      * right or wrong
-
-    * Display partial word guessed by the user and use underscore in place of not guess word    
-    '''
     print("Welcome to the game, Hangman!")
     print("I am thinking of a word that is {} letters long.".format(
         str(len(secret_word))), end='\n\n')
 
     letters_guessed = []
+    remaining_lives=8
+    no_of_wrong_inputs=0
 
-    available_letters = get_available_letters(letters_guessed)
-    print("Available letters: {} ".format(available_letters))
+    while remaining_lives>0:
+        print("You have "+str(remaining_lives)+" lives remaining.")
+        available_letters = get_available_letters(letters_guessed)
+        print("Available letters: {} ".format(available_letters))
 
-    guess = input("Please guess a letter: ")
-    letter = guess.lower()
+        guess = input("Please guess a letter: ")
+        letter = guess.lower()
 
-    if letter in secret_word:
-        letters_guessed.append(letter)
-        print("Good guess: {} ".format(
-            get_guessed_word(secret_word, letters_guessed)))
-        if is_word_guessed(secret_word, letters_guessed) == True:
-            print(" * * Congratulations, you won! * * ", end='\n\n')
-    else:
-        print("Oops! That letter is not in my word: {} ".format(
-            get_guessed_word(secret_word, letters_guessed)))
-        letters_guessed.append(letter)
-        print("")
+        if not isValid(letter):
+            print("INVALID INPUT, FUCK OFF!!")
+            continue
+
+        if letter in secret_word:
+            letters_guessed.append(letter)
+            print("Good guess: {} ".format(
+                get_guessed_word(secret_word, letters_guessed)))
+            if is_word_guessed(secret_word, letters_guessed) == True:
+                print(" * * Congratulations, you won! * * ", end='\n\n')
+        else:
+            print("Oops! That letter is not in my word: {} ".format(
+                get_guessed_word(secret_word, letters_guessed)))
+            remaining_lives-=1
+            no_of_wrong_inputs+=1
+            print_image(no_of_wrong_inputs)
+            letters_guessed.append(letter)
+            print("")
 
 
-# Load the list of words into the variable wordlist
-# So that it can be accessed from anywhere in the program
 secret_word = choose_word()
 hangman(secret_word)
